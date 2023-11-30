7ae67913b6cc6afa613e2f7405c67db8e0e15ab2
mb <115391907+FireHeart3198@users.noreply.github.com>
2023-12-01 10:52:29 +1100

    Add buttons and eventListener for numbers

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 let firstNumber
 let operator
 let secondNumber
+let currentEquation = []
 
 function add (firstNumber, secondNumber) {
     return firstNumber + secondNumber;
@@ -18,3 +19,24 @@ function divide (firstNumber, secondNumber) {
     return firstNumber / secondNumber;
 }
 
+function operate (operator, firstNumber, secondNumber) {
+    switch(operator) {
+        case "add":
+            return add (firstNumber, secondNumber);
+        case "subtract":
+            return subtract (firstNumber, secondNumber);
+        case "multiply":
+            return multiply (firstNumber, secondNumber);
+        case "divide":
+            return divide (firstNumber, secondNumber);
+    }
+}
+
+numberButtons = document.querySelector(".numbers");
+
+numberButtons.addEventListener('click', function(e) {
+    if (!(e.target.className==="numbers")) {
+        currentEquation.push(e.target.textContent);
+        console.log(currentEquation);
+    }
+})
