2b864e3ed61c930cd2b05b4c0f0ba4f8aa392aea
mb <115391907+FireHeart3198@users.noreply.github.com>
2023-12-01 02:31:39 +1100

    Add basic math operator functions

---
+++ javascript.js
@@ -0,0 +1,20 @@
+let firstNumber
+let operator
+let secondNumber
+
+function add (firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function subtract (firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function multiply (firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+}
+
+function divide (firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+}
+
